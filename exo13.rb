e08694a744ed6ad10e041ec59813814de7408d28
Sabine60 <86677488+Sabine60@users.noreply.github.com>
2021-07-06 23:51:34 +0200

    Add files via upload

---
+++ exo13.rb
@@ -0,0 +1,14 @@
+i= 1
+email_list = Array.new()
+        while(i <= 50)
+            if(i <= 50)  
+                if (i < 10)  
+                    email = "sara.dupont.0#{i}@email.fr"
+                else 
+                    email = "sara.dupont.0#{i}@email.fr"
+                end
+                
+                email_list.push(email)
+                i = i + 1
+            end
+            puts email_list
